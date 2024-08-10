1093b382cb0341571ae7e0fb1284f10d60b75f88
SitanHuang <978494543.qq54@gmail.com>
2024-08-09 17:05:05 -0700

    dev: impl & test CacheManager

---
+++ gui/graphics/cacheManager.js
@@ -0,0 +1,176 @@
+
+/**
+ * Manages caching and dirty states of arbitrary objects in a tree structure
+ *
+ * Data structure:
+ *
+ *  ex. calling getObj(1, 2) traverses map as follows:
+ *
+ *  Map
+ *   -> Map (key1 = ex. "1" for tile row)
+ *     -> Map (key2 = ex. "2" for tile col)
+ *       -> "self"
+ */
+class CacheManager {
+  static SYM_SELF = Symbol("self");
+  static SYM_DIRTY = Symbol("dirty");
+  static SYM_KEYS = Symbol("keys");
+  static SYM_PARENT_MAP = Symbol("parentMap");
+
+  dirtyQueue = new Array();
+
+  store;
+  config;
+
+  log;
+
+  constructor(config) {
+    this.store = new Map();
+
+    this.config = Object.assign({
+      name: "gui.graphics.cachemanager",
+    }, config);
+
+    this.log = Logger.get(this.config.name);
+  }
+
+  getFreshObj(...keys) {
+    const con = this.#getContainerMap(keys, true, true);
+    const obj = con?.get(CacheManager.SYM_SELF);
+
+    if (con?.get(CacheManager.SYM_DIRTY))
+      throw new Error(`accessing a dirty object: ${keys}`);
+
+    return obj || null;
+  }
+
+  getObjForce(...keys) {
+    const con = this.#getContainerMap(keys, true, true);
+    const obj = con?.get(CacheManager.SYM_SELF);
+
+    return obj || null;
+  }
+
+  /**
+   * refreshes all dirty objects by traversing recusively the dirty Maps
+   *
+   * @param {function} callback the async callback func to receive each dirty obj
+   */
+  async refreshDirtyObjsAsync(refreshFunc = async function (_keys, _obj) {}, ...targetKeysPrefix) {
+    const timeLabel = "refreshDirtyObjsAsync, queue size=" + this.dirtyQueue.length;
+    this.log.time(timeLabel);
+
+    for (let i = 0; i < this.dirtyQueue.length; i++) {
+      const map = this.dirtyQueue[i];
+      const keys = map.get(CacheManager.SYM_KEYS);
+
+      if (targetKeysPrefix.length && !this.#matchKeys(keys, targetKeysPrefix))
+        continue;
+
+      if (!map.get(CacheManager.SYM_DIRTY))
+        continue;
+
+      if (map.has(CacheManager.SYM_SELF))
+        await refreshFunc(keys, map.get(CacheManager.SYM_SELF));
+
+      map.set(CacheManager.SYM_DIRTY, false);
+
+      map.forEach((val, key) => {
+        if (key === CacheManager.SYM_DIRTY ||
+          key === CacheManager.SYM_KEYS ||
+          key === CacheManager.SYM_SELF ||
+          key === CacheManager.SYM_PARENT_MAP ||
+          !(val instanceof Map))
+          return;
+
+        this.dirtyQueue.push(val);
+      });
+    }
+
+    this.dirtyQueue = this.dirtyQueue.filter(x => x.get(CacheManager.SYM_DIRTY));
+
+    this.log.timeEnd(timeLabel);
+
+    return this;
+  }
+
+  addFreshObj(obj, ...keys) {
+    this.#getContainerMap(keys, true)
+      .set(CacheManager.SYM_SELF, obj)
+      .set(CacheManager.SYM_DIRTY, false);
+
+    return this;
+  }
+
+  setFreshKey(...keys) {
+    this.#getContainerMap(keys, true)
+      .set(CacheManager.SYM_DIRTY, false);
+
+    return this;
+  }
+
+  setDirty(...keys) {
+    // set entire map as dirty
+    this.dirtyQueue.push(
+      this.#getContainerMap(keys, true)
+        .set(CacheManager.SYM_DIRTY, true)
+    );
+
+    return this;
+  }
+
+  deleteContainer(...keys) {
+    this.#removeFromDirtyQueue(keys);
+    this.#getContainerMap(keys, false, false)
+      ?.get(CacheManager.SYM_PARENT_MAP)
+      ?.delete(keys.at(-1));
+    return this;
+  }
+
+  #removeFromDirtyQueue(targetKeysPrefix) {
+    this.dirtyQueue = this.dirtyQueue.filter(map => {
+      const keys = map.get(CacheManager.SYM_KEYS);
+
+      return this.#matchKeys(keys, targetKeysPrefix);
+    });
+  }
+
+  #matchKeys(keys, targetKeysPrefix) {
+    if (!keys)
+      return false;
+
+    for (let i = 0; i < targetKeysPrefix.length; i++) {
+      if (targetKeysPrefix[i] !== keys[i])
+        return false;
+    }
+
+    return true;
+  }
+
+  #getContainerMap(keys, autoCreate=false, throwOnDirtyParent=false) {
+    let parent = this.store;
+
+    const path = [];
+    for (const key of keys) {
+      if (autoCreate)
+        path.push(key);
+
+      if (throwOnDirtyParent && parent.get(CacheManager.SYM_DIRTY))
+        throw new Error(`accessing a dirty parent: key=${keys}, dirtyKey=${parent.get(CacheManager.SYM_KEYS)}`);
+
+      let child = parent.get(key);
+      if (!child) {
+        if (autoCreate) {
+          parent.set(key, child = new Map());
+          child.set(CacheManager.SYM_KEYS, [...path]);
+          child.set(CacheManager.SYM_PARENT_MAP, parent);
+        } else {
+          return null;
+        }
+      }
+      parent = child;
+    }
+
+    return parent;
+  }
+}
---
+++ test/spec/gui.graphics.cachemanager.test.js
@@ -0,0 +1,111 @@
+
+describe('CacheManager', function () {
+  let cacheManager;
+
+  beforeEach(() => {
+    cacheManager = new CacheManager();
+  });
+
+  describe("addFreshObj and getFreshObj", () => {
+    const testObj = { id: 1 };
+
+    beforeEach(() => {
+      cacheManager = new CacheManager();
+      cacheManager.addFreshObj(testObj, "key1", "key2");
+    });
+
+    it("should add and retrieve a fresh object", () => {
+      expect(cacheManager.getFreshObj("key1")).toBeNull();
+      expect(cacheManager.getFreshObj("key1", "key2")).toBe(testObj);
+    });
+
+    it("should throw an error when trying to access a dirty key", () => {
+      cacheManager.setDirty("key1");
+      expect(() => cacheManager.getFreshObj("key1", "key2")).toThrowError("accessing a dirty parent: key=key1,key2, dirtyKey=key1");
+    });
+
+    it("should throw an error when trying to access a dirty object", () => {
+      cacheManager.setDirty("key1", "key2");
+      expect(() => cacheManager.getFreshObj("key1", "key2")).toThrowError("accessing a dirty object: key1,key2");
+    });
+  });
+
+  describe("setDirty and refreshDirtyObjsAsync", () => {
+    it("should mark an object as dirty and then refresh it", async () => {
+      cacheManager.addFreshObj({ id: 1, data: "original" }, "key1", "key2");
+      cacheManager.addFreshObj({ id: 2, data: "original" }, "key1", "key3");
+      cacheManager.addFreshObj({ id: 3, data: "original" }, "key4", "key5");
+      cacheManager.setDirty("key1", "key2");
+
+      expect(() => cacheManager.getFreshObj("key1", "key2")).toThrowError("accessing a dirty object: key1,key2");
+      expect(cacheManager.getFreshObj("key1", "key3").data).toEqual("original");
+      expect(cacheManager.getFreshObj("key4", "key5").data).toEqual("original");
+
+      cacheManager.setDirty("key1");
+
+      expect(() => cacheManager.getFreshObj("key1", "key2")).toThrowError("accessing a dirty parent: key=key1,key2, dirtyKey=key1");
+      expect(() => cacheManager.getFreshObj("key1", "key3")).toThrowError("accessing a dirty parent: key=key1,key3, dirtyKey=key1");
+      expect(cacheManager.getFreshObj("key4", "key5").data).toEqual("original");
+
+      cacheManager.setDirty("key4");
+
+      expect(() => cacheManager.getFreshObj("key4", "key5")).toThrowError("accessing a dirty parent: key=key4,key5, dirtyKey=key4");
+
+      await cacheManager.refreshDirtyObjsAsync(async (keys, obj) => {
+        expect(keys[0]).toEqual("key1");
+        expect(keys[1]).toEqual("key2");
+        expect(keys.length).toEqual(2);
+        obj.data = "updated";
+      }, "key1", "key2");
+
+      expect(() => cacheManager.getFreshObj("key4", "key5")).toThrowError("accessing a dirty parent: key=key4,key5, dirtyKey=key4");
+
+      cacheManager.setFreshKey("key1");
+      cacheManager.setFreshKey("key4");
+      cacheManager.setDirty("key4", "key5");
+
+      expect(cacheManager.getFreshObj("key1", "key2").data).toEqual("updated");
+      expect(cacheManager.getFreshObj("key1", "key3").data).toEqual("original");
+
+      cacheManager.setDirty("key1", "key3");
+      expect(() => cacheManager.getFreshObj("key1", "key3")).toThrowError("accessing a dirty object: key1,key3");
+      expect(() => cacheManager.getFreshObj("key4", "key5")).toThrowError("accessing a dirty object: key4,key5");
+
+      await cacheManager.refreshDirtyObjsAsync(async (keys, obj) => {
+        expect(keys[0]).toEqual("key1");
+        expect(keys[1]).toEqual("key3");
+        expect(keys.length).toEqual(2);
+        expect(obj.data).toEqual("original");
+        obj.data = "updated";
+      }, "key1");
+
+      expect(cacheManager.getFreshObj("key1", "key2").data).toEqual("updated");
+      expect(cacheManager.getFreshObj("key1", "key3").data).toEqual("updated");
+    });
+
+    it("should only refresh specified dirty objects based on targetKeysPrefix", async () => {
+      cacheManager.addFreshObj({ id: 1 }, "key1");
+      cacheManager.addFreshObj({ id: 2 }, "key1", "key2");
+      cacheManager.addFreshObj({ id: 3 }, "key3");
+      cacheManager.setDirty("key1");
+      cacheManager.setDirty("key1", "key2");
+      cacheManager.setDirty("key3");
+
+      await cacheManager.refreshDirtyObjsAsync(async (keys, obj) => {
+        obj.id += 100;
+      }, "key1");
+
+      expect(cacheManager.getFreshObj("key1").id).toBe(101);
+      expect(cacheManager.getFreshObj("key1", "key2").id).toBe(102);
+      expect(cacheManager.getObjForce("key3").id).toBe(3); // Not refreshed
+    });
+  });
+
+  describe("deleteContainer", () => {
+    it("should delete a container and its object should not be accessible", () => {
+      cacheManager.addFreshObj({ id: 1 }, "key1", "key2");
+      cacheManager.deleteContainer("key1", "key2");
+      expect(cacheManager.getFreshObj("key1", "key2")).toBeNull();
+    });
+  });
+});
